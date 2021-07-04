ef730d17d3495886c926cc95236bb66296c45039
Oleg Bovtunov <obovtunov@gmail.com>
2021-07-04 22:06:49 +0300

    t

--- temp/static.sh
+++ temp/static.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t hometask-image .
-docker run -t -d -p 8081:80 --name www hometask-image
-docker cp index.html www /var/www/html
+docker run --rm -t -d -p 8081:80 --name www hometask-image
+docker cp index.html www:/var/www/html
